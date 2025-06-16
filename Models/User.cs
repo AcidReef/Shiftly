1939af8bbe58a3331f099d38cbcc34dd6984fef6
Michael <119465676+AcidReef@users.noreply.github.com>
2025-06-16 15:52:14 +0200

    test

--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,6 @@ public class User
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Role { get; set; }
